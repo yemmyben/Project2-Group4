12b7f29bab38caf94c6b2a0772dc9899f911c978
irfanwalele <irfanwalele@gmail.com>
2019-08-03 14:01:42 -0400

    updated 2

--- models/example.js
+++ models/example.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Example = sequelize.define("Tackle", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT,
     price: DataTypes.INTEGER
